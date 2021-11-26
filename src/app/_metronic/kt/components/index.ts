72a8e70742b2d4d18e49790a842b52c7f735524e
spectum <sc.apichatt@gmail.com>
2021-11-26 21:01:42 +0700

    remove some css

--- src/app/_metronic/kt/components/index.ts
+++ src/app/_metronic/kt/components/index.ts
@@ -1,11 +1,7 @@
 export * from './_CookieComponent'
 export * from './_DrawerComponent'
-export * from './_FeedbackComponent'
 export * from './_ScrollComponent'
 export * from './_StickyComponent'
 export * from './_ToggleComponent'
-// export * from './_DialerComponent';
-export * from './_PasswordMeterComponent'
-export * from './_PlaceComponent'
 export * from './MenuComponent'
 export * from './SearchComponent'
